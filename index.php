792b4557932bc9806bdbd488d8e9526eb0dbbe4b
Pingkon Augustine Rozario <pingkonaugustineroz@gmail.com>
2022-08-22 13:53:19 +0600

    index update

--- index.php
+++ index.php
@@ -22,14 +22,23 @@ include("include/header.php");
         <div class="col-md-12">
             <div class="row">
                 <div class="col-md-3 mx-3 shadow">
-                    <img src="images/doctor.png" alt="doctor" style="width: 100%;">
+                    <img src="images/patient.png" alt="patient" style="width: 100%;">
+
+                    <h5 class="text-center">Create an account to for care!</h5>
+
+                    <a href="">
+                        <button class="btn btn-success my-3" style="margin-left: 35%;">Sign Up</button>
+                    </a>
                 </div>
                 <div class="col-md-3 mx-3 shadow">
-                    <img src="images/patient.png" alt="patient" style="width: 100%;">
+                    <img src="images/doctor.png" alt="doctor" style="width: 100%;">
+
+                    <h5 class="text-center">We are hiring for doctors!</h5>
+
+                    <a href="">
+                        <button class="btn btn-success my-3" style="margin-left: 35%;">Apply Now</button>
+                    </a>
                 </div>
-                <!-- <div class="col-md-3 mx-3 shadow">
-                    <img src="" alt="">
-                </div> -->
             </div>
         </div>
     </div>
