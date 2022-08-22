af0d7f436a9edb01845de5357d8691ee76d7d1fa
Pingkon Augustine Rozario <pingkonaugustineroz@gmail.com>
2022-08-22 10:22:36 +0600

    first commit

---
+++ include/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
+
+    <script src="https://code.jquery.com/jquery-3.6.0.slim.js"
+        integrity="sha256-HwWONEZrpuoh951cQD1ov2HUK5zA5DwJ1DNUXaM6FsY=" crossorigin="anonymous"></script>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+    <link rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.1.2/css/fontawesome.min.css">
+
+    <title>HMS</title>
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-info bg-info">
+        <h5 class="text-white">Hospital Management System</h5>
+
+        <div class="mr-auto"></div>
+
+        <ul class="">
+            <li></li>
+        </ul>
+    </nav>
+
+</body>
+
+</html>
